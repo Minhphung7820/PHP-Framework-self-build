5e1021483fae587c4ceb29b7577614976301f45d
Trương Minh Phung <minhphung485@gmail.com>
2023-06-03 11:34:09 +0700

    HTTP

--- Supports/Facades/Base/Http.php
+++ Supports/Facades/Base/Http.php
@@ -4,7 +4,11 @@ namespace Supports\Facades\Base;
 
 abstract class Http
 {
-    abstract public static function get();
-    abstract public static function post();
-    abstract public static function withHeaders();
+    abstract public static function get($url, $data = []);
+    abstract public static function post($url, $data = []);
+    abstract public static function put($url, $data = []);
+    abstract public static function patch($url, $data = []);
+    abstract public static function delete($url, $data = []);
+    abstract public static function withHeaders(array $headers);
+    abstract protected static function setHeaders($curl);
 }
--- Supports/Facades/Http.php
+++ Supports/Facades/Http.php
@@ -2,6 +2,94 @@
 
 namespace Supports\Facades;
 
-class Http
+use Supports\Facades\Base\Http as BaseHttp;
+
+class Http extends BaseHttp
 {
+    protected static $headers = [];
+
+    public static function get($url, $data = [])
+    {
+        $queryString = http_build_query($data);
+        $url = $url . '?' . $queryString;
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        self::setHeaders($curl);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return $response;
+    }
+
+    public static function post($url, $data = [])
+    {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        self::setHeaders($curl);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return $response;
+    }
+
+    public static function put($url, $data = [])
+    {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        self::setHeaders($curl);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return $response;
+    }
+
+    public static function patch($url, $data = [])
+    {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        self::setHeaders($curl);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return $response;
+    }
+
+    public static function delete($url, $data = [])
+    {
+        $queryString = http_build_query($data);
+        $url = $url . '?' . $queryString;
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        self::setHeaders($curl);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return $response;
+    }
+
+    public static function withHeaders(array $headers)
+    {
+        self::$headers = $headers;
+        return new self();
+    }
+
+    protected static function setHeaders($curl)
+    {
+        if (!empty(self::$headers)) {
+            $formattedHeaders = [];
+            foreach (self::$headers as $key => $value) {
+                $formattedHeaders[] = $key . ': ' . $value;
+            }
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $formattedHeaders);
+        }
+    }
 }
